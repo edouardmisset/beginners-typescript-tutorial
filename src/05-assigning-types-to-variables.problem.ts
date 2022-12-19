d1d17b693fd014dd988f6f6a283c33e6830c45cc
Edouard Misset <edouard.misset@intest.info>
2022-12-20 00:40:14 +0100

    Solved Exercise 05

--- src/05-assigning-types-to-variables.problem.ts
+++ src/05-assigning-types-to-variables.problem.ts
@@ -1,22 +1,25 @@
-import { expect, it } from "vitest";
+import { expect, it } from 'vitest'
 
 interface User {
-  id: number;
-  firstName: string;
-  lastName: string;
-  isAdmin: boolean;
+  id: number
+  firstName: string
+  lastName: string
+  isAdmin: boolean
 }
 
 /**
  * How do we ensure that defaultUser is of type User
  * at THIS LINE - not further down in the code?
  */
-const defaultUser = {};
+const defaultUser: User = {
+  firstName: 'Bat',
+  id: 1,
+  isAdmin: true,
+  lastName: 'Man',
+}
 
-const getUserId = (user: User) => {
-  return user.id;
-};
+const getUserId = (user: User) => user.id
 
-it("Should get the user id", () => {
-  expect(getUserId(defaultUser)).toEqual(1);
-});
+it('Should get the user id', () => {
+  expect(getUserId(defaultUser)).toEqual(1)
+})
