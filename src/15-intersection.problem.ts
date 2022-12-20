c56cb25ead676b59731f9ff040a95f778ac988b3
Edouard Misset <edouard.misset@intest.info>
2022-12-20 01:02:12 +0100

    Solved Exercise 15

--- src/15-intersection.problem.ts
+++ src/15-intersection.problem.ts
@@ -1,34 +1,32 @@
 interface User {
-  id: string;
-  firstName: string;
-  lastName: string;
+  id: string
+  firstName: string
+  lastName: string
 }
 
 interface Post {
-  id: string;
-  title: string;
-  body: string;
+  id: string
+  title: string
+  body: string
 }
 
 /**
  * How do we type this return statement so it's both
  * User AND { posts: Post[] }
  */
-export const getDefaultUserAndPosts = (): unknown => {
-  return {
-    id: "1",
-    firstName: "Matt",
-    lastName: "Pocock",
-    posts: [
-      {
-        id: "1",
-        title: "How I eat so much cheese",
-        body: "It's pretty edam difficult",
-      },
-    ],
-  };
-};
+export const getDefaultUserAndPosts = (): User & { posts: Post[] } => ({
+  id: '1',
+  firstName: 'Matt',
+  lastName: 'Pocock',
+  posts: [
+    {
+      id: '1',
+      title: 'How I eat so much cheese',
+      body: "It's pretty edam difficult",
+    },
+  ],
+})
 
-const userAndPosts = getDefaultUserAndPosts();
+const userAndPosts = getDefaultUserAndPosts()
 
-console.log(userAndPosts.posts[0]);
+console.log(userAndPosts.posts[0])
